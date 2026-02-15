99ef53f65a66377405398152ead9cabd1443bf8a
kensaku aoki <bellend.loves.old.skool@gmail.com>
2026-02-15 12:53:01 +0900

    chore(auth): switch Cognito domain prefix to photohub4kansa

--- frontend/config.js
+++ frontend/config.js
@@ -2,7 +2,7 @@ window.KANSA_CONFIG = {
   apiBase: 'https://0nhz8sat7j.execute-api.ap-northeast-1.amazonaws.com',
   photoBucket: 'kansa-backend-photobucket-ufurvgtp4oqi',
   cognitoRegion: 'ap-northeast-1',
-  cognitoDomain: 'photohub4kansa-auth-478860598832',
+  cognitoDomain: 'photohub4kansa',
   cognitoClientId: 'nlpccd2h79jr93rejmiv35eci',
   cognitoRedirectUri: window.location.origin,
 };
