cde440e3fee11faa861032dc659dce104b9294e9
kensaku aoki <bellend.loves.old.skool@gmail.com>
2026-04-09 08:06:27 +0900

    make demo

--- frontend/landing.js
+++ frontend/landing.js
@@ -1,6 +1,8 @@
 (function () {
   var appButton = document.getElementById("to-app");
   var legalButton = document.getElementById("to-legal");
+  var demoButtons = [document.getElementById("to-demo-top"), document.getElementById("to-demo-bottom")];
+
   if (appButton) {
     appButton.addEventListener("click", function () {
       window.location.href = "./index.html";
@@ -11,4 +13,12 @@
       window.location.href = "./legal.html";
     });
   }
+
+  demoButtons.forEach(function (button) {
+    if (button) {
+      button.addEventListener("click", function () {
+        window.location.href = "./demo.html";
+      });
+    }
+  });
 })();
