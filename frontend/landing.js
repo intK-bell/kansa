66427a9fb13f21fb5f1f9eedbcc86db349c135db
kensaku aoki <bellend.loves.old.skool@gmail.com>
2026-05-10 13:06:11 +0900

    nonclarityURLpram of  LP

--- frontend/landing.js
+++ frontend/landing.js
@@ -1,26 +1,37 @@
 (function () {
-  var clarityId = "wiygdr6cwn";
-  window.clarity =
-    window.clarity ||
-    function () {
-      (window.clarity.q = window.clarity.q || []).push(arguments);
-    };
+  var params = new URLSearchParams(window.location.search);
+  var disableClarity = params.has("noclarity");
 
-  var clarityScript = document.createElement("script");
-  clarityScript.async = true;
-  clarityScript.src = "https://www.clarity.ms/tag/" + clarityId;
-  var firstScript = document.getElementsByTagName("script")[0];
-  firstScript.parentNode.insertBefore(clarityScript, firstScript);
+  if (!disableClarity) {
+    var clarityId = "wiygdr6cwn";
+
+    window.clarity =
+      window.clarity ||
+      function () {
+        (window.clarity.q = window.clarity.q || []).push(arguments);
+      };
+
+    var clarityScript = document.createElement("script");
+    clarityScript.async = true;
+    clarityScript.src = "https://www.clarity.ms/tag/" + clarityId;
+
+    var firstScript = document.getElementsByTagName("script")[0];
+    firstScript.parentNode.insertBefore(clarityScript, firstScript);
+  }
 
   var appButton = document.getElementById("to-app");
   var legalButton = document.getElementById("to-legal");
-  var demoButtons = [document.getElementById("to-demo-top"), document.getElementById("to-demo-bottom")];
+  var demoButtons = [
+    document.getElementById("to-demo-top"),
+    document.getElementById("to-demo-bottom"),
+  ];
 
   if (appButton) {
     appButton.addEventListener("click", function () {
       window.location.href = "./index.html";
     });
   }
+
   if (legalButton) {
     legalButton.addEventListener("click", function () {
       window.location.href = "./legal.html";
